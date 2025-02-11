92d423a62a784d2227f43f6da7f933bc7306afcc
Arya Wiratama <aryawiratama2401@gmail.com>
2025-02-11 15:00:48 +0700

    add framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import './App.css'
-
+import { motion } from 'framer-motion'
 
 function Navbar() {
   return (
     <header className='header'>
-
-      <div className='logo'>
-        <img src="/assets/logo-zodia.png" alt="" />
+      <motion.div 
+        className='logo'
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <img src="/assets/logo-zodia.png" alt="Logo Zodia" />
         <h1>Zodia</h1>
-      </div>
+      </motion.div>
 
       <nav className='navbar'>
         <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">Katalog</a></li>
-          <li><a href="">Our Medsos</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Katalog</a></li>
+          <li><a href="#">Our Medsos</a></li>
         </ul>
       </nav>
     </header>
@@ -22,9 +26,7 @@ function Navbar() {
 }
 
 function App() {
-  return (
-    Navbar()
-  )
+  return <Navbar />  
 }
 
 export default App
