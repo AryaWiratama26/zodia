1a5eac7ca090ed5256d40fe8940a20dec42973ad
Arya Wiratama <aryawiratama2401@gmail.com>
2025-02-11 14:19:30 +0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import './App.css'
 
+
+function Navbar() {
+  return (
+    <header>
+
+      <div>
+        {/* <img src="/" alt="" /> */}
+        <h1>Zodia</h1>
+      </div>
+
+      <nav>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Katalog</a></li>
+          <li><a href="">Our Medsos</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    Navbar()
   )
 }
 
